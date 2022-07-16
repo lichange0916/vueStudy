c96ad40ce0354e2442772a76cdc176bfb076a73d
mark.li <863806366@qq.com>
2022-07-16 22:13:10 +0800

    Upload the file of test

---
+++ 19、单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+       <school></school>
+       <student/>
+    </div>
+</template>
+
+<script>
+  // 引入组件
+  import School from './School.vue'
+  import Student from './Student.vue'
+
+  export default {
+      name:'app',
+      components:{
+        School,
+        Student,
+      }
+  }
+</script>
---
+++ 19、单文件组件/School.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="demo">
+        <h2>学校名称{{schoolName}}</h2>
+        <h2>学校地址{{address}}</h2>
+        <button @click="showName">点我提示学校名称</button>
+   </div>
+</template>
+
+<script>
+    export default{
+        name:'School',
+        data(){
+            return {
+                schoolName:'尚硅谷',
+                address:'北京昌平',
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            }
+        }
+    }
+    
+</script>
+
+<style>
+   .demo{
+    background-color: orange;
+   }
+</style>
